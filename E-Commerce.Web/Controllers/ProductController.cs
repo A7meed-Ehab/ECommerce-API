a9cd8d2f6b99ca29a89a56c33fbf8e3d7ef564d3
aramadan-amcham <aramadan@amcham.org.eg>
2025-12-01 15:45:41 +0200

    Add project files.

---
+++ E-Commerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace E_Commerce.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("GetById/{id:int}")]
+        public ActionResult<Product> GetById([FromRoute]int id)
+        {
+
+            return new Product() { Id= id, Name="Rice"};
+        }
+        [HttpPost]
+        public ActionResult<Product> CreateProduct(Product product)
+        {
+            return product;
+        }
+    }
+}
